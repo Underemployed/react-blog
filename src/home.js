4e6d1d0b157e88c01f9496996e5bac399343e18c
Underemployed <underemployed871@gmail.com>
2025-08-04 20:50:51 +0530

    usestate

--- src/home.js
+++ src/home.js
@@ -1,7 +1,31 @@
-const Home = () => {
+import { useState } from "react";
+
+
+
+const Home = () => {    
+    // let name = "baaaaaaaaaaaaaaaa";
+    // const handleClick = () => {
+    //     name = (name === "luigi") ? "baaaa" : "luigi"
+    //     console.log(name)
+    // }
+    
+    const [name, setName] = useState("mario");
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName("luigi")
+        setAge(31)
+    }
+
+
+    
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <h2>{name} is {age}yrs old</h2>
+            <button onClick={
+                handleClick
+            }>CLick me</button>
         </div>
     );
 }
