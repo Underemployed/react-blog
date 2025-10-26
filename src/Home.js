31659a1fa5dbfbfcecb945ca681af95447b35882
Underemployed <underemployed871@gmail.com>
2025-10-26 11:00:36 +0530

    useState delete components passing variables

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,24 @@
 import { useState } from "react";
-
+import BlogList from "./blogList";
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { id: 1, title: "Wut", body: "Lorem ipsum wtvr", author: "Mario" },
+        { id: 2, title: "Why", body: "Another blog body", author: "Luigi" },
+        { id: 3, title: "How", body: "Yet another blog post", author: "Yoshi" },
+        { id: 4, title: "When", body: "More blog content here", author: "Peach" },
+        { id: 5, title: "Where", body: "Final blog post content", author: "Mario" }
+    ]);
 
-    let [name, setName] = useState("mario");
-    
-    let [age, setAge] = useState(18);
 
-    function handleClick() {
-        name = (name === "me") ? "Escape" : "me";
-        setName(name)
-        console.log("Name changed to ", name)
-
-        setAge(age+10)
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((x) => x.id !== id);
+        setBlogs(newBlogs);
     }
+
     return ( 
         <div className="home">
-            <h2>HomePage</h2>
-            <p>{name} is { age} old</p>
-            <button onClick={handleClick}>Update name</button>
+            <BlogList blogs={ blogs} handleDelete={handleDelete} title= "All Blogs"/>
+            <BlogList blogs={ blogs.filter((x)=> (x.author === "Mario"))}  title= "Mario's Blogs"/>
         </div>
      );
 }
