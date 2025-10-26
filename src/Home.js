f91043731c12578d88dd4a3803c3460e55f4e6d8
Underemployed <underemployed871@gmail.com>
2025-10-26 12:12:55 +0530

    load data json server using useEffect npx json-server --watch data/db.json --port 8000

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,29 @@
 import { useEffect, useState } from "react";
 import BlogList from "./blogList";
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { id: 1, title: "Wut", body: "Lorem ipsum wtvr", author: "Mario" },
-        { id: 2, title: "Why", body: "Another blog body", author: "Luigi" },
-        { id: 3, title: "How", body: "Yet another blog post", author: "Yoshi" },
-        { id: 4, title: "When", body: "More blog content here", author: "Peach" },
-        { id: 5, title: "Where", body: "Final blog post content", author: "Mario" }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const [name, setName] = useState("luigi");
-
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((x) => x.id !== id);
-        setBlogs(newBlogs);
-    }
     // can be used to fetch data
     // npx json-server --watch data/db.json --port 8000
     useEffect(() => {
         console.log("Runs when dom changes?");
-        console.log(name);
+        fetch("http://localhost:8000/blogs").then(
+            (response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setBlogs(data);
+            })
         // dont change variables  state
         // state change -> dom change -> call useeffect -> state change
         // loop dont update state here
-    },[name]) 
+    },[]) 
     // dependency array variable tracker 
     //  only runs once
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs" />
-            <button onClick={()=>setName("Mario")}>Change Name</button>
-            <p>Name: {name}</p>
+            {/*  logical and evaluates left first. if its false it doesnt check right */}
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}         
         </div>
 
      );
